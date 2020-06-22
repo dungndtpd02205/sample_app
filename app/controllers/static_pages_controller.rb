7f92fd47b0d8699fa1a127a87f413dd767c85282
dungndtpd02205 <ndtdunggg@gmail.com>
2020-06-22 08:10:22 +0700

    Chapter_3+4+5 :Mostly static pages + RAILS-FLAVORED RUBY + Filling in the layout

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,23 @@
+class StaticPagesController < ApplicationController
+  before_action :set_locale
+
+  def home; end
+
+  def help; end
+
+  def about; end
+
+  def contact; end
+
+  private
+
+  def set_locale
+    locale = params[:locale].to_s.strip.to_sym
+    I18n.locale = I18n.available_locales.include?(locale) ?
+    locale : I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+end
